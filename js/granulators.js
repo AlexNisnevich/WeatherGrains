152fd621e0bb67ff5f218f85c29b43b291333612
Brien Southward <brien@Briens-iMac.local>
2016-10-01 22:41:33 -0700

    updated polyphony parameters

--- js/granulators.js
+++ js/granulators.js
@@ -6,7 +6,7 @@ var granulators = [
       params.detune = mapRange(weather.main.temp, 273, 310, 1200, -1200, true);
       params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 0.5);
       params.attack = mapRange(weather.wind.speed, 0, 50, 0.5, 0.1, true);
-      params.interval = mapRange(weather.clouds.all, 0, 100, 0.3, 0.1);
+      params.interval = mapRange(weather.clouds.all, 0, 100, 0.4, 0.1);
       params.spread = mapRange(weather.main.pressure, 700, 1100, 0.01, 0.1, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0, 0.25);
       params.azimuth = weather.wind.deg;
@@ -17,8 +17,8 @@ var granulators = [
     file: 'audio/birdsong1.wav',
     paramsFromWeather: function (params, weather) {
       params.detune = mapRange(weather.main.temp, 273, 310, -1200, 1200, true);
-      params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 0.5);
-      params.attack = mapRange(weather.wind.speed, 0, 50, 0.5, 0.1, true);
+      params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 1.0);
+      params.attack = mapRange(weather.wind.speed, 0, 50, 1.0, 0.1, true);
       params.interval = mapRange(weather.clouds.all, 0, 100, 0.5, 0.1);
       params.spread = mapRange(weather.main.pressure, 700, 1100, 0.01, 0.1, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0, 0.25);
@@ -34,7 +34,7 @@ var granulators = [
       params.attack = mapRange(weather.wind.speed, 0, 50, 0.4, 0.1, true);
       params.interval = mapRange(weather.clouds.all, 0, 100, 0.5, 0.1);
       params.spread = mapRange(weather.main.pressure, 700, 1100, 0.01, 0.1, true);
-      params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0, 0.25);
+      params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0, 0.5);
       params.azimuth = weather.wind.deg;
     }
   }/*,
