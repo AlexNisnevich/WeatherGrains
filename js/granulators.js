b69b543014a3c49c5aeaffbc81bd05df1a8c9625
Brien Southward <brien@Briens-iMac.local>
2016-10-20 19:32:30 -0700

    Updated parameters

--- js/granulators.js
+++ js/granulators.js
@@ -3,11 +3,11 @@ var granulators = [
   {
     file: 'audio/cello-c3.wav',
     paramsFromWeather: function (params, weather) {
-      params.detune = Math.round(mapRange(weather.main.temp, 191, 331, -1200, 1200, true));
-      params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 0.5);
+      params.detune = Math.round(mapRange(weather.main.temp, 251, 331, -1200, 1200, true));
+      params.release = mapRange(weather.main.humidity, 0, 100, 0.4, 1.0);
       params.attack = mapRange(weather.wind.speed, 0, 100, 0.7, 0.1, true);
-      params.interval = mapRange(weather.clouds.all, 0, 100, 0.2, 0.6);
-      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.01, 0.2, true);
+      params.interval = mapRange(weather.clouds.all, 0, 100, 0.6, 0.2);
+      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.3, 0.1, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0.25, 0.01, true);
       params.azimuth = weather.wind.deg;
     }
@@ -16,11 +16,11 @@ var granulators = [
   {
     file: 'audio/birdsong1.wav',
     paramsFromWeather: function (params, weather) {
-      params.detune = mapRange(weather.main.temp, 229, 331, -1200, 1200, true);
+      params.detune = mapRange(weather.main.temp, 251, 331, -1200, 1200, true);
       params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 1.0);
       params.attack = mapRange(weather.wind.speed, 0, 100, 1.0, 0.2, true);
       params.interval = mapRange(weather.clouds.all, 0, 100, 0.5, 0.1);
-      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.01, 0.2, true);
+      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.3, 0.01, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0.01, 0.25, true);
       params.azimuth = weather.wind.deg;
     }
@@ -29,11 +29,11 @@ var granulators = [
   {
     file: 'audio/violin-a4.wav',
     paramsFromWeather: function (params, weather) {
-      params.detune = Math.round(mapRange(weather.main.temp, 191, 331, -1200, 1200, true));
+      params.detune = Math.round(mapRange(weather.main.temp, 251, 317, 1200, -1200, true));
       params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 0.4);
-      params.attack = mapRange(weather.wind.speed, 0, 100, 0.5, 0.1, true);
-      params.interval = mapRange(weather.clouds.all, 0, 100, 1.0, 0.1);
-      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.01, 0.2, true);
+      params.attack = mapRange(weather.wind.speed, 0, 100, 0.7, 0.1, true);
+      params.interval = mapRange(weather.clouds.all, 0, 100, 0.1, 0.5);
+      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.3, 0.1, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0.01, 0.25, true);
       params.azimuth = weather.wind.deg;
     }
@@ -42,11 +42,11 @@ var granulators = [
   {
     file: 'audio/violin-a4.wav',
     paramsFromWeather: function (params, weather) {
-      params.detune = Math.round(mapRange(weather.main.temp, 193, 317, -1200, 1200, true));
+      params.detune = Math.round(mapRange(weather.main.temp, 251, 331, -1200, 1200, true));
       params.release = mapRange(weather.main.humidity, 0, 100, 0.1, 0.5);
-      params.attack = mapRange(weather.wind.speed, 0, 100, 0.3, 0.1, true);
-      params.interval = mapRange(weather.clouds.all, 0, 100, 1.0, 0.2);
-      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.01, 0.2, true);
+      params.attack = mapRange(weather.wind.speed, 0, 100, 0.4, 0.05, true);
+      params.interval = mapRange(weather.clouds.all, 0, 100, 0.7, 0.2);
+      params.spread = mapRange(weather.main.pressure, 500, 1300, 0.3, 0.1, true);
       params.randomization = mapRange(weather.sys.sunset - weather.sys.sunrise, 0, 86400, 0.02, 0.5, true);
      params.azimuth = weather.wind.deg;
     }
